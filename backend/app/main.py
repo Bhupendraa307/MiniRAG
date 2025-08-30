cc44d6ffb929d36ca664af617d4b053f283990d8
Bhupendra kumar chouhan <bhupendrajdh31@gmail.com>
2025-08-30 23:11:30 +0530

    Update main.py
    cors origin - https://mini-rag-rosy.vercel.app/

--- backend/app/main.py
+++ backend/app/main.py
@@ -28,7 +28,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Configure appropriately for production
+    allow_origins=["https://mini-rag-rosy.vercel.app/"],  # Configure appropriately for production
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
